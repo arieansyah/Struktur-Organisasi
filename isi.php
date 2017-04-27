7d5b4ac1f4a46df71a45376fbb7ea5eb0b134919
arieansyah <arieansyahp.bun@gmail.com>
2017-04-27 07:39:24 +0700

    page edit

--- isi.php
+++ isi.php
@@ -1,1 +1,17 @@
-<h1>oke</h1>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Halaman Edit</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">  	
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h1 class="text-center">Halaman Edit</h1>
+			</div>
+			
+		</div>
+	</div>
+</body>
+</html>
